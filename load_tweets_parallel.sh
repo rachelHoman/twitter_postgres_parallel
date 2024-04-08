6cd3657c7b53890e1420914406002a0ad156a209
rachelHoman <rachelmarie001@aol.com>
2024-04-07 17:47:24 -0700

    fixed time

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,12 +5,14 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel sh ./load_denormalized.sh
+echo "$files" | time parallel sh load_denormalized.sh
+
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2363/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2363/ --inputs
+
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3363/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3363/ --inputs
